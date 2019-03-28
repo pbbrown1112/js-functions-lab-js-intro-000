e19acee3dc7dbe25e53b827ea427f57c65d0bedf
Patrick B Brown <pbbrown1112@gmail.com>
2019-03-28 21:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function happyHolidays(){
+function happyHolidays() {
   return "Happy holidays!"
 }
 
 // write your code below!
+function happyHoldaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
